4701898ec344da9cac067c4d34c77b197113be29
Pavlentiy <vskyhlam@gmail.com>
2023-05-27 20:56:17 +0700

    constructors and toString

--- src/main/java/org/example/University.java
+++ src/main/java/org/example/University.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.apache.logging.log4j.message.StringFormattedMessage;
+
 public class University {
 
     String id;
@@ -7,4 +9,42 @@ public class University {
     String shortName;
     int yearOfFoundation;
     StudyProfile mainProfile;
+
+    public University(String id, String fullName, String shortName, int yearOfFoundation, StudyProfile mainProfile) {
+        this.id = id;
+        this.fullName = fullName;
+        this.shortName = shortName;
+        this.yearOfFoundation = yearOfFoundation;
+        this.mainProfile = mainProfile;
+    }
+
+    public String toString() {
+        return "University{"
+                + "fullName='" + fullName + '\''
+                + ", shortName=" + shortName
+                + ", yearOfFoundation=" + yearOfFoundation
+                + ", mainProfile=" + mainProfile
+                + '}';
+    }
+    public String getId() {return id;}
+
+    public void setId(String id) {this.id = id;}
+
+    public String getFullName() {return fullName;}
+
+    public void setFullName(String fullName) {this.fullName = fullName;}
+
+    public String getShortName() {return shortName;}
+
+    public void setShortName(String shortName) {this.shortName = shortName;}
+
+    public int getYearOfFoundation() {return yearOfFoundation;}
+
+    public void setYearOfFoundation(int yearOfFoundation) {this.yearOfFoundation = yearOfFoundation;}
+
+    public StudyProfile getMainProfile() {return mainProfile;}
+
+    public void setMainProfile(StudyProfile mainProfile) {this.mainProfile = mainProfile;}
+
+
 }

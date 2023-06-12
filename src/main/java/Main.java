39c7cf193e6f632587f01823b8c4b019f0f24f8b
Pavlentiy <vskyhlam@gmail.com>
2023-06-12 22:59:45 +0700

    В методе main при помощи Java Stream API реализован вывод в консоль информации о студентах в порядке увеличения оценки и информации об университетах в алфавитном порядке по сокращенному названию

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,11 @@
 import classes.FileReaderUtil;
 import classes.Student;
 import classes.University;
+import comparators.StudentComparator;
+import comparators.UniversityComparator;
+import enumClasses.ComparatorFactory;
+import enumClasses.StudentEnum;
+import enumClasses.UniversityEnum;
 
 import java.io.IOException;
 import java.util.List;
@@ -9,16 +14,18 @@ public class Main {
 
     public static void main(String[] args)throws IOException {
 
-            FileReaderUtil fileReader = FileReaderUtil.getInstance();
+        FileReaderUtil fileReader = FileReaderUtil.getInstance();
 
-            List<Student> studentsList = fileReader.readStudentsFromFile("universityInfo.xlsx");
-            for (Student student : studentsList) {
-                System.out.println(student);
-            }
+        //Вывод информации о студентах в порядке возрастания оценок
+        StudentComparator studentComparator = ComparatorFactory.getStudentComparator(StudentEnum.AVG_EXAM_SCORE);
+        List<Student> studentsList = fileReader.readStudentsFromFile("universityInfo.xlsx");
 
-            List<University> universitiesList = fileReader.readUniversitiesFromFile("universityInfo.xlsx");
-            for (University university : universitiesList) {
-                System.out.println(university);
-            }
+        studentsList.stream().sorted(studentComparator).forEach(System.out::println);
+
+        //Вывод информации об университетах в алфавитном порядке по сокращенному названию
+        UniversityComparator universityComparator = ComparatorFactory.getUniversityComparator(UniversityEnum.SHORT_NAME);
+        List<University> universitiesList = fileReader.readUniversitiesFromFile("universityInfo.xlsx");
+
+        universitiesList.stream().sorted(universityComparator).forEach(System.out::println);
     }
 }
---
+++ src/main/java/enumClasses/ComparatorFactory.java
@@ -0,0 +1,24 @@
+package enumClasses;
+
+import comparators.*;
+
+public class ComparatorFactory {
+    public static StudentComparator getStudentComparator(StudentEnum studentEnum) {
+        return switch (studentEnum) {
+            case FULL_NAME -> new StFullNameComparator();
+            case ID -> new StIDComparator();
+            case COURSE_NUMBER -> new StCourseNumberComparator();
+            case AVG_EXAM_SCORE -> new StAvgExamNumberComparator();
+        };
+    }
+
+    public static UniversityComparator getUniversityComparator(UniversityEnum universityEnum) {
+        return switch (universityEnum) {
+            case ID -> new UniIDComparator();
+            case FULL_NAME -> new UniFullNameComparator();
+            case SHORT_NAME -> new UniShortNameComparator();
+            case YEAR_OF_FOUNDATION -> new UniYearOfFoundationComparator();
+            case STUDY_PROFILE -> new UniStudyProfileComparator();
+        };
+    }
+}
